54406198370e9e8853382a4866f3ca0adda23c21
Derek <derekdanielson84@gmail.com>
2023-02-27 11:47:07 -0700

    commit

--- app.js
+++ app.js
@@ -31,3 +31,51 @@ if (typeof id !== 'undefined'){
     console.log('No ID');
 }
 
+
+//Logical Operators
+const name = 'Bob';
+const age = 20;
+
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
+}
+
+
+// Ternary Operator
+const id = 100;
+
+console.log(id === 100 ? 'Correct' : 'Incorrect');
+
+
+// Without braces
+if (id === 100)
+    console.log('Correct');
+else
+    console.log('Incorrct');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
